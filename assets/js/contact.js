651fcf5fcea8d75a9f5b42c961c1dba43f730206
Cleanidbogor <cleanidbogo@gmail.com>
2024-07-17 07:26:32 +0700

    push

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,29 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publicKey
+    // Ini Data Dami
+    emailjs.sendForm('service_8ii265v','template_85g6ftk','#contact-form','VQaq6RG_bXgUd7BNm')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully '
+
+            // Remove message after five second
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (services error)'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
